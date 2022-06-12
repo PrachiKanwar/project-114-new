6d25ca6644d60d1e445c2e6ea12843a01c41851e
PrachiKanwar <92808164+PrachiKanwar@users.noreply.github.com>
2022-06-12 20:45:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300)
     canvas.center()
+    video=createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on("pose",gotposes)
+}
+function modelloaded(){
+    console.log("posenet is initialized")
 }
 function draw(){
-
+image (video,0,0,300,300)
 }
 function take_snapshot(){
     save("prachi.png")
 
+}
+function gotposes(results){
+if(results.length>0){
+    console.log(results)
+    console.log("nosex= "+results[0].pose.nose.x)
+    console.log("nosey= "+results[0].pose.nose.y)
+}
 }
